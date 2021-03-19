d91cc9b0110a89c5106f3fc7aaef4cf88ec3ed4c
Sam McGarey <mcgareysj@wustl.edu>
2021-03-19 14:30:33 -0500

    added basic student functionality

---
+++ src/manyRubrics/Student.java
@@ -0,0 +1,29 @@
+package manyRubrics;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student {
+	
+	private String studentName;
+	private Map<Assignment, Double> assigmentsToScores;
+	
+	public Student(String name, List<Assignment> assignments) {
+		this.studentName = name;
+		this.assigmentsToScores = new HashMap<Assignment, Double>();
+		
+		for(Assignment assignment : assignments) {
+			this.assigmentsToScores.put(assignment, 0.0);
+		}
+	}
+	
+	public void addScoreToAssignment(Assignment assignmentWithScore, double score) {
+		this.assigmentsToScores.replace(assignmentWithScore, score);
+	}
+	
+	public double getScoreForAssignment(Assignment assignmentToRetrieveScoreFor) {
+		return this.assigmentsToScores.get(assignmentToRetrieveScoreFor);
+	}
+
+}
