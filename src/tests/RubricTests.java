27bb5e2eb6ca6283eee52c822b71e8ff63539bfd
Angelica Harris <40836454+angelicaharris@users.noreply.github.com>
2021-03-23 00:18:21 -0500

    Added rubric copy constructor test

--- src/tests/RubricTests.java
+++ src/tests/RubricTests.java
@@ -20,17 +20,16 @@ class RubricTests {
 		assignments.add(firstAssignment);
 		assignments.add(secondAssignment);
 
-		
 		Rubric rubricUnderTest = new Rubric(assignments);
 		rubricUnderTest.setAssignmentWeight(firstAssignment, 50);
 		rubricUnderTest.setAssignmentWeight(secondAssignment, 50);
 
 		boolean isValid = rubricUnderTest.isValid();
-		
+
 		assertTrue(isValid);
 
 	}
-	
+
 	@Test
 	void testInvalidRubric() {
 		ArrayList<Assignment> assignments = new ArrayList<Assignment>();
@@ -38,41 +37,64 @@ class RubricTests {
 		Assignment secondAssignment = new Assignment("second");
 		assignments.add(firstAssignment);
 		assignments.add(secondAssignment);
-		
-		
+
 		Rubric rubricUnderTest = new Rubric(assignments);
 		rubricUnderTest.setAssignmentWeight(firstAssignment, 10);
 		rubricUnderTest.setAssignmentWeight(secondAssignment, 10);
 
 		boolean isInvalid = rubricUnderTest.isValid();
-		
+
 		assertFalse(isInvalid);
 
 	}
-	
+
 	@Test
 	void testRubricComputeScores() {
 		ArrayList<Assignment> assignments = new ArrayList<Assignment>();
-		
+
 		Assignment firstAssignment = new Assignment("first");
-		
+
 		Assignment secondAssignment = new Assignment("second");
 		assignments.add(firstAssignment);
 		assignments.add(secondAssignment);
-		
+
 		Student student = new Student("Test Student", assignments);
 		student.addScoreToAssignment(firstAssignment, 100);
 		student.addScoreToAssignment(secondAssignment, 60);
 
-		
+		Rubric rubricUnderTest = new Rubric(assignments);
+		rubricUnderTest.setAssignmentWeight(firstAssignment, 50);
+		rubricUnderTest.setAssignmentWeight(secondAssignment, 50);
+
+		double score = rubricUnderTest.computeScoreForStudent(student);
+
+		assertEquals(80.0, score);
+
+	}
+
+	@Test
+	void testRubricCopyConstructor() {
+		ArrayList<Assignment> assignments = new ArrayList<Assignment>();
+		Assignment firstAssignment = new Assignment("first");
+
+		Assignment secondAssignment = new Assignment("second");
+		assignments.add(firstAssignment);
+		assignments.add(secondAssignment);
+
+		Student student = new Student("Test Student", assignments);
+		student.addScoreToAssignment(firstAssignment, 100);
+		student.addScoreToAssignment(secondAssignment, 60);
+
 		Rubric rubricUnderTest = new Rubric(assignments);
 		rubricUnderTest.setAssignmentWeight(firstAssignment, 50);
 		rubricUnderTest.setAssignmentWeight(secondAssignment, 50);
 		
-		double score = rubricUnderTest.computeScoreForStudent(student);
+		Rubric copy = new Rubric(rubricUnderTest);
+		double score = copy.computeScoreForStudent(student);
 		
-		assertEquals(80.0, score);
 
+		assertEquals(80.0, score);
+		
 	}
 
 }
