b6e541e209be7e38198efcf7a0bb2b27558c699e
Angelica Harris <40836454+angelicaharris@users.noreply.github.com>
2021-04-27 20:10:18 -0500

    rankingRubric

---
+++ src/manyRubrics/rankingStudents.java
@@ -0,0 +1,26 @@
+package manyRubrics;
+
+import java.util.Collections;
+import java.util.List;
+
+public class rankingStudents {
+
+	//list of students 
+	//sort students based on calculated grades
+	private List<Student>listOfStudents;
+	private Rubric rubric;
+	
+	public rankingStudents(List<Student> listOfStudents,Rubric rubric) {
+		this.listOfStudents =  listOfStudents;
+		this.rubric = rubric; 
+	}
+	
+	public List<Student> RankStudent(){
+		Collections.sort(listOfStudents, (Student student1, Student student2)->{
+			double score1 = rubric.computeScoreForStudent(student1);
+			double score2 = rubric.computeScoreForStudent(student2);
+			return score1<score2 ? 1:(score1>score2 ?-1:0);
+		});
+		return listOfStudents;
+	}
+}
---
+++ src/tests/rankingStudentTests.java
@@ -0,0 +1,51 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import manyRubrics.Assignment;
+import manyRubrics.Rubric;
+import manyRubrics.Student;
+
+public class rankingStudentTests {
+
+	@Test
+	void testRankStudent() {
+		ArrayList<Assignment> assignments = new ArrayList<Assignment>();
+		Assignment firstAssignment = new Assignment("first", 100);
+		Assignment secondAssignment = new Assignment("second", 100);
+		assignments.add(firstAssignment);
+		assignments.add(secondAssignment);
+
+		
+		Rubric rubricUnderTest = new Rubric(assignments);
+		rubricUnderTest.setAssignmentWeight(firstAssignment, 50);
+		rubricUnderTest.setAssignmentWeight(secondAssignment, 50);
+
+		Student student1 = new Student("AName", assignments);
+		student1.addScoreToAssignment(firstAssignment, 75);
+		student1.addScoreToAssignment(secondAssignment, 75);
+		
+		Student student2 = new Student("BName", assignments);
+		student2.addScoreToAssignment(firstAssignment, 90);
+		student2.addScoreToAssignment(secondAssignment, 90);
+		
+		Student student3 = new Student("CName", assignments);
+		student3.addScoreToAssignment(firstAssignment, 90);
+		student3.addScoreToAssignment(secondAssignment, 75);
+		
+		Student student4 = new Student("DName", assignments);
+		student4.addScoreToAssignment(firstAssignment, 70);
+		student4.addScoreToAssignment(secondAssignment, 70);
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(student1);
+		students.add(student2);
+		students.add(student3);
+		students.add(student4);
+	}
+}
